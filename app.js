5b33a69324d4f450a302e97eed13e431e070c8b6
SamPatt <git@sampatt.com>
2023-10-24 15:15:00 -0400

    Initial commit; finished Lab Part 1

---
+++ app.js
@@ -0,0 +1,27 @@
+// Menu data structure
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+// Task 1
+let mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+mainEl.classList.add('flex-ctr');
+
+// Task 2
+let topMenuEl = document.getElementById('top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+// Task 3
+for (link of menuLinks) {
+    let aEl = document.createElement('a');
+    aEl.setAttribute('href', link.href)
+    aEl.innerText = link.text
+    topMenuEl.appendChild(aEl)
+}
